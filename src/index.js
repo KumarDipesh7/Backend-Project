c8c61a7509739bf59bfd1411fc006e0eed9466de
unknown <deekr777@gmail.com>
2025-02-11 13:59:16 +0530

    Database is connected

---
+++ src/index.js
@@ -0,0 +1,34 @@
+// require ('dotenv').config({path:`./env`})
+import dotenv from "dotenv"
+// import mongoose from "mongoose";
+//database ka jab bhi use kro toh try catch block ka use kro kyuki error aa skta hai and async await ka use kro kyuki response aane me time lg skta hai
+// import {DB_NAME} from "./constants";
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path: `./env`
+})
+
+connectDB()
+
+
+/*
+import express from "express"
+const app=express()
+
+(async ()=>{
+    try{
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        app.on("error",(error)=>{
+            console.log("Error",error);
+            throw error
+        })
+        app.listen(process.env.PORT,()=>{
+            console.log(`App is listening on PORT ${process.env.PORT}`)
+        })
+    } catch(error){
+        console.error("ERROR",error)
+        throw err
+    }
+})()
+    */
